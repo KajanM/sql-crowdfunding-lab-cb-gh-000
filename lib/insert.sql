54adabf5c3e509b7925aed37d22e5cf5284984ad
KajanM <kajan.14@cse.mrt.ac.lk>
2017-06-11 10:42:29 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,272 @@
+INSERT INTO projects(title)
+VALUES (
+  'one'
+);
+
+INSERT INTO projects(title)
+VALUES (
+  'two'
+);
+
+INSERT INTO projects(title)
+VALUES (
+  'three'
+);
+
+INSERT INTO projects(title)
+VALUES (
+  'four'
+);
+
+INSERT INTO projects(title)
+VALUES (
+  'five'
+);
+
+INSERT INTO projects(title)
+VALUES (
+  'six'
+);
+
+INSERT INTO projects(title)
+VALUES (
+  'seven'
+);
+
+INSERT INTO projects(title)
+VALUES (
+  'eight'
+);
+
+INSERT INTO projects(title)
+VALUES (
+  'nine'
+);
+
+INSERT INTO projects(title)
+VALUES (
+  'ten'
+);
+
+INSERT INTO users(name)
+VALUES (
+  'user1'
+);
+
+INSERT INTO users(name)
+VALUES (
+  'user2'
+);
+
+INSERT INTO users(name)
+VALUES (
+  'user3'
+);
+
+INSERT INTO users(name)
+VALUES (
+  'user4'
+);
+
+INSERT INTO users(name)
+VALUES (
+  'user5'
+);
+
+INSERT INTO users(name)
+VALUES (
+  'user6'
+);
+
+INSERT INTO users(name)
+VALUES (
+  'user7'
+);
+
+INSERT INTO users(name)
+VALUES (
+  'user8'
+);
+
+INSERT INTO users(name)
+VALUES (
+  'user9'
+);
+
+INSERT INTO users(name)
+VALUES (
+  'user10'
+);
+
+INSERT INTO users(name)
+VALUES (
+  'user11'
+);
+INSERT INTO users(name)
+VALUES (
+  'user12'
+);
+INSERT INTO users(name)
+VALUES (
+  'user13'
+);
+INSERT INTO users(name)
+VALUES (
+  'user14'
+);
+INSERT INTO users(name)
+VALUES (
+  'user15'
+);
+INSERT INTO users(name)
+VALUES (
+  'user16'
+);
+INSERT INTO users(name)
+VALUES (
+  'user17'
+);
+INSERT INTO users(name)
+VALUES (
+  'user18'
+);
+INSERT INTO users(name)
+VALUES (
+  'user19'
+);
+INSERT INTO users(name)
+VALUES (
+  'user20'
+);
+
+INSERT INTO pledges(amount)
+VALUES (
+  1
+);
+
+INSERT INTO pledges(amount)
+VALUES (
+  2
+);
+INSERT INTO pledges(amount)
+VALUES (
+  3
+);
+INSERT INTO pledges(amount)
+VALUES (
+  4
+);
+INSERT INTO pledges(amount)
+VALUES (
+  5
+);
+
+INSERT INTO pledges(amount)
+VALUES (
+  6
+);
+
+INSERT INTO pledges(amount)
+VALUES (
+  7
+);
+INSERT INTO pledges(amount)
+VALUES (
+  8
+);
+INSERT INTO pledges(amount)
+VALUES (
+  9
+);
+INSERT INTO pledges(amount)
+VALUES (
+  10
+);
+
+INSERT INTO pledges(amount)
+VALUES (
+  11
+);
+
+INSERT INTO pledges(amount)
+VALUES (
+  12
+);
+INSERT INTO pledges(amount)
+VALUES (
+  13
+);
+INSERT INTO pledges(amount)
+VALUES (
+  14
+);
+INSERT INTO pledges(amount)
+VALUES (
+  15
+);
+
+INSERT INTO pledges(amount)
+VALUES (
+  16
+);
+
+INSERT INTO pledges(amount)
+VALUES (
+  17
+);
+INSERT INTO pledges(amount)
+VALUES (
+  18
+);
+INSERT INTO pledges(amount)
+VALUES (
+  19
+);
+INSERT INTO pledges(amount)
+VALUES (
+  20
+);
+
+INSERT INTO pledges(amount)
+VALUES (
+  21
+);
+
+INSERT INTO pledges(amount)
+VALUES (
+  22
+);
+INSERT INTO pledges(amount)
+VALUES (
+  23
+);
+INSERT INTO pledges(amount)
+VALUES (
+  24
+);
+INSERT INTO pledges(amount)
+VALUES (
+  25
+);
+
+INSERT INTO pledges(amount)
+VALUES (
+  26
+);
+
+INSERT INTO pledges(amount)
+VALUES (
+  27
+);
+INSERT INTO pledges(amount)
+VALUES (
+  28
+);
+INSERT INTO pledges(amount)
+VALUES (
+  29
+);
+INSERT INTO pledges(amount)
+VALUES (
+  30
+);
